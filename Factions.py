918883e9999af85dab6ce6b160c5ed1e8ca515af
FynnGnu <fynngnugesser@gmail.com>
2025-02-01 15:09:14 -0500

    Created initial entity and faction classes

--- Factions.py
+++ Factions.py
@@ -1,4 +1,5 @@
 import random
+from Entities import Entity
 
 class Faction:
     name = ""
@@ -10,7 +11,12 @@ class Faction:
     aggression = 0
     status = ""
 
+    births = 0
+    deaths = 0
+    oldest = 0
+
     DEATH_MULTIPLIER = 0.75
+    REPRODUCTION_MULTIPLIER = 0.25
 
     def __init__(self, name, area = 0, membersCount = 0):
         self.name = name
@@ -19,9 +25,12 @@ class Faction:
         self.members = []
         self.status = "active"
 
-    def addMember(self, ent):
+    def addMember(self):
+        ent = Entity(faction = self.name)
         self.members.append(ent)
         self.membersCount += 1
+        if self.status == "inactive":
+            self.status = "active"
 
     def calculateFactionStats(self):
         self.brain = 0
@@ -34,12 +43,30 @@ class Faction:
                 self.aggression += ent.aggression
 
     def incrementAge(self):
+        self.births = 0
+        self.deaths = 0
+        self.oldest = 0
         for ent in self.members:
             if ent.status == "alive":
                 ent.age += 1
-                if ent.age >= 50:
+                if ent.age >= 45:
                     deathChance = ent.age * self.DEATH_MULTIPLIER
                     deathRoll = random.randint(1, 100)
-                    if deathRoll > deathChance:
+                    if deathChance > deathRoll:
                         ent.status = "dead"
                         self.membersCount -= 1
+                        self.deaths += 1
+                        if self.membersCount == 0:
+                            self.status = "inactive"
+            if ent.status == "alive":
+                if ent.age >= 20:
+                    reproductionRoll = random.randint(1, 100)
+                    if reproductionRoll < self.REPRODUCTION_MULTIPLIER * 100:
+                        self.births += 1
+                if ent.age > self.oldest:
+                    self.oldest = ent.age
+        for x in range(self.births):
+            self.addMember()
+
+    def printFactionStats(self):
+        print("Name: ", self.name, ", Status: ", self.status, ", Members: ", self.membersCount, ", New Births: ", self.births, ", New Deaths: ", self.deaths, ", Net Change: ", self.births - self.deaths, ", Oldest Member: ", self.oldest)
--- main.py
+++ main.py
@@ -8,16 +8,28 @@ fact3 = Faction(name="Green")
 
 factions = [fact1, fact2, fact3]
 
-def ageTimer(a):
+for x in range(10):
+    for faction in factions:
+        faction.addMember()
+
+
+def ageTimer(a, message):
     while True:
         time.sleep(a)
+        message(a)
         for faction in factions:
             faction.incrementAge()
             faction.calculateFactionStats()
+            faction.printFactionStats()
 
 def ageMessage(a):
-    print("{} minutes have passed. Increasing all entity ages by one...".format(a/60))
+    print("\n\n{} minutes have passed. Increasing all entity ages by one...".format(a/60))
 
 if __name__ == '__main__':
-    ageThread = threading.Thread(target = ageTimer, args = (60, ageMessage(60)), daemon = True)
+    ageThread = threading.Thread(target = ageTimer, args = (1, ageMessage), daemon = True)
+    ageThread.start()
+
+    while True:
+        print("Main program is running...")
+        time.sleep(60)  # Simulating other tasks
 
